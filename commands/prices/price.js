acaf2d25c5f874a9aff825fdb12fe738155daba3
NTX8205 <ad0124578963@gmail.com>
2023-05-28 23:38:46 +0800

    新增斜線指令 /price

---
+++ commands/prices/price.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { getEmbedFromExchange } = require('../../src/embed')
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('price')
+        .setDescription('查看當前價格'),
+    async execute(interaction) {
+        const finalEmbed = await getEmbedFromExchange();
+        await interaction.reply({ embeds: [finalEmbed] });
+    },
+};
