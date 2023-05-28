f7af5d20c9842aa0d72701507e59abb5cf4dd695
NTX8205 <ad0124578963@gmail.com>
2023-05-28 23:37:33 +0800

    將連結更改 模板重新修改

--- src/embed.js
+++ src/embed.js
@@ -11,24 +11,47 @@ module.exports.getEmbedFromExchange = async function getEmbedFromExchange() {
     const formatExchange = exchange[0].map(e => e + 'c').toString();
 
     return new EmbedBuilder()
-        .setDescription('目前1D換C的價格，取官網前10筆')
+        .setAuthor({
+            name: "目前POE台服價格",
+        })
+        .setColor("#ffbe0a")
         .setTitle('官網連結')
-        .setURL(`https://web.poe.garena.tw/trade/exchange/卡蘭德/${exchange[1]}`)
+        .setURL(`https://web.poe.garena.tw/trade/exchange/熔火聯盟/${exchange[1]}`)
+        .setDescription('目前 1D換C 的價格，取官網前 10 筆\n每 4 分鐘將會定時送出訊息至特定頻道')
         .addFields(
+            {
+                name: "目前版本、聯盟",
+                value: "3.21 版  熔火冥獄",
+                inline: true
+            },
+            {
+                name: "使用方法",
+                value: "</help:1112299719394340897>查看說明\n</price:1112209071601365124>或 `!d` 查看價格",
+                inline: true
+            },
             { name: '第 1 ~ 10 筆', value: formatExchange },
             { name: '換算成c:', value: '取第二筆為基準作換算，小數點無條件捨去' },
-            { name: '\u200b', 
-            value: `0.1D = ${Math.trunc(exchange[0][1] * 0.1).toString() + 'c' } \n 
-                    0.2D = ${Math.trunc(exchange[0][1] * 0.2).toString() + 'c' } \n
-                    0.3D = ${Math.trunc(exchange[0][1] * 0.3).toString() + 'c' } \n 
-                    0.4D = ${Math.trunc(exchange[0][1] * 0.4).toString() + 'c' } \n 
-                    0.5D = ${Math.trunc(exchange[0][1] * 0.5).toString() + 'c' } \n `
-                    , inline: true},
-            { name: '\u200b', 
-            value: `0.6D = ${Math.trunc(exchange[0][1] * 0.6).toString() + 'c'} \n 
-                    0.7D = ${Math.trunc(exchange[0][1] * 0.7).toString() + 'c'} \n 
-                    0.8D = ${Math.trunc(exchange[0][1] * 0.8).toString() + 'c'} \n 
-                    0.9D = ${Math.trunc(exchange[0][1] * 0.9).toString() + 'c'} \n ` 
-                    , inline: true },
-            );
+            {
+                name: '\u200b',
+                value: ` 0.1D = ${Math.trunc(exchange[0][1] * 0.1).toString() + 'c'}
+                        \n0.2D = ${Math.trunc(exchange[0][1] * 0.2).toString() + 'c'}
+                        \n0.3D = ${Math.trunc(exchange[0][1] * 0.3).toString() + 'c'}
+                        \n0.4D = ${Math.trunc(exchange[0][1] * 0.4).toString() + 'c'} 
+                        \n0.5D = ${Math.trunc(exchange[0][1] * 0.5).toString() + 'c'}`
+                , inline: true
+            },
+            {
+                name: '\u200b',
+                value: `0.6D = ${Math.trunc(exchange[0][1] * 0.6).toString() + 'c'}
+                        \n0.7D = ${Math.trunc(exchange[0][1] * 0.7).toString() + 'c'}
+                        \n0.8D = ${Math.trunc(exchange[0][1] * 0.8).toString() + 'c'}
+                        \n0.9D = ${Math.trunc(exchange[0][1] * 0.9).toString() + 'c'}`
+                , inline: true
+            },
+        )
+        .setFooter({
+            text: "資料取得時間",
+        })
+        .setTimestamp();
+
 }
