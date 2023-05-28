33d4ddbbdf47b115a4aca7151ae9fa1fdd09c2fc
NTX8205 <ad0124578963@gmail.com>
2023-05-28 23:38:53 +0800

    新增斜線指令 /help

---
+++ commands/info/help.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { botInfo } = require('../../src/info');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('查看使用方法'),
+    async execute(interaction) {
+        const helpInfo = await botInfo();
+        await interaction.reply({ embeds: [helpInfo] });
+    },
+};
